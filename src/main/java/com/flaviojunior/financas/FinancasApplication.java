267838e50743d68f955a42883a980ec33625cc4b
flaviofonseca-smartnx <flavio.fonseca@smartnx.com>
2022-09-16 00:57:08 -0300

    ajustes do cors

--- src/main/java/com/flaviojunior/financas/FinancasApplication.java
+++ src/main/java/com/flaviojunior/financas/FinancasApplication.java
@@ -2,10 +2,20 @@ package com.flaviojunior.financas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class FinancasApplication {
+@EnableWebMvc
+public class FinancasApplication implements WebMvcConfigurer {
 
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		
+		registry.addMapping("/**").allowedMethods("GET","PUT","POST","DELETE","OPTIONS");
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(FinancasApplication.class, args);
 	}
--- src/main/java/com/flaviojunior/financas/api/dto/UsuarioDTO.java
+++ src/main/java/com/flaviojunior/financas/api/dto/UsuarioDTO.java
@@ -1,13 +1,19 @@
 package com.flaviojunior.financas.api.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UsuarioDTO {
+	
+	
 	
 	private String email;
 	private String nome;
